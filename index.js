b1133bdcb4525cbb80243f357e9b3da445439583
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-10-25 21:50:51 -0500

    Init DNS feature and put logic to save short urls

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const dns = require('dns');
 let bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
@@ -6,6 +7,11 @@ const app = express();
 
 // Basic Configuration
 const port = 3000;
+
+let last_saved_id = 0;
+let saved_short_urls = {
+  0: "localhost:3000"
+}
 
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -22,13 +28,30 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+
 app.post("/api/shorturl", function(req, res){
   console.log(req.body)
-  res.json({ original_url : req.body.url, short_url : 1})
+
+  dns.resolve(req.body.url, (err, address, family) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('address: %j family: IPv%s', address, family);
+
+    saved_short_urls[last_saved_id+1] = req.body.url;
+    last_saved_id++;
+    res.json({ original_url : req.body.url, short_url : 1})
+  });
+});
+
+app.get("/api/shorturl/status", function(req, res){
+  res.json(saved_short_urls)
 });
 
 app.get("/api/shorturl/:short_url", function(req, res){
   console.log(req.params)
+  console.log("Will be redirected to " + saved_short_urls[req.params.short_url])
   res.json({ redirect_to : req.params.short_url})
 });
 
